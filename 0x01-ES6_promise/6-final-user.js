edc335c37071afe3ba4f563d6ceed41d8d69b0d5
fabrizia2 <melvinrenish56@gmail.com>
2023-04-20 00:57:56 +0300

    final-user

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,26 +1,17 @@
-import signUpUser from './4-user-promise';
+import signUpcreatedUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = [];
-  
-  const userPromise = signUpUser(firstName, lastName)
-    .then((data) => {
-      return { status: 'fulfilled', value: data };
-    })
-    .catch((error) => {
-      return { status: 'rejected', value: error };
+  const promise = [];
+  try {
+    const User = await signUpcreatedUser(firstName, lastName);
+    promise.push({ status: 'fulfilled', value: User });
+    await uploadPhoto(fileName);
+  } catch (err) {
+    promise.push({
+      status: 'rejected',
+      value: `Error: ${fileName} cannot be processed`,
     });
-
-  const photoPromise = uploadPhoto(fileName)
-    .then(() => {
-      return { status: 'fulfilled', value: 'Photo uploaded' };
-    })
-    .catch((error) => {
-      return { status: 'rejected', value: error };
-    });
-    
-  promises.push(userPromise, photoPromise);
-
-  return Promise.all(promises);
+  }
+  return promise;
 }
