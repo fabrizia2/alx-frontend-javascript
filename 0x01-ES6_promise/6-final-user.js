837c3a9e4c1c3cf96cc40ca6621c5d96ccf62a23
fabrizia2 <melvinrenish56@gmail.com>
2023-04-20 00:51:21 +0300

    final-user

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,26 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const promises = [];
+  
+  const userPromise = signUpUser(firstName, lastName)
+    .then((data) => {
+      return { status: 'fulfilled', value: data };
+    })
+    .catch((error) => {
+      return { status: 'rejected', value: error };
+    });
+
+  const photoPromise = uploadPhoto(fileName)
+    .then(() => {
+      return { status: 'fulfilled', value: 'Photo uploaded' };
+    })
+    .catch((error) => {
+      return { status: 'rejected', value: error };
+    });
+    
+  promises.push(userPromise, photoPromise);
+
+  return Promise.all(promises);
+}
