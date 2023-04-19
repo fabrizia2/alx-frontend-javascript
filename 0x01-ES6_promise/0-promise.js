c23d70f771458814c8d9dd6bb38001b0d290ae85
fabrizia2 <melvinrenish56@gmail.com>
2023-04-19 23:40:21 +0300

    Promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,7 +1,6 @@
-function getResponseFromAPI() {
+export default function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
-    // Make API call asynchronously
-    // Once the call is completed, resolve the Promise with the response data
-    // If there is an error, reject the Promise with an error object
+	  resolve();
+	  reject();
   });
 }
