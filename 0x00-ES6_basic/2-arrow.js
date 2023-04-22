ca4cefae2c8ce3fa3ce53edd64725df786b5c4f8
fabrizia2 <melvinrenish56@gmail.com>
2023-04-22 19:23:11 +0300

    block-scoped

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,10 +1,10 @@
 export default function getNeighborhoodsList() {
-  // This function is intended to be used as a method of a class, 
-  // where `this` refers to the instance of the class.
-  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];   
-
-  this.addNeighborhood = (newNeighborhood) => {
-    this.sanFranciscoNeighborhoods.push(newNeighborhood);        
-    return this.sanFranciscoNeighborhoods;
+  const obj = {
+    sanFranciscoNeighborhoods: ['SOMA', 'Union Square'],
+    addNeighborhood(newNeighborhood) {
+      this.sanFranciscoNeighborhoods.push(newNeighborhood);
+      return this.sanFranciscoNeighborhoods;
+    }
   };
+  return obj;
 }
