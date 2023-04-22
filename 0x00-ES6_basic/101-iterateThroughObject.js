38d9bfe70d24e6c1f3c4fb462774973453b4d3a7
fabrizia2 <melvinrenish56@gmail.com>
2023-04-22 22:23:17 +0300

    The wonders

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,14 +1,7 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let result = '';
-  let currentDepartment = reportWithIterator.next().value;
-  while (currentDepartment) {
-    const employees = currentDepartment[1];
-    for (const employee of employees) {
-      result += employee + ' | ';
-    }
-    currentDepartment = reportWithIterator.next().value;
+  let employees = '';
+  for (const employee of reportWithIterator) {
+    employees += `${employee} | `;
   }
-  // Remove the trailing ' | ' characters
-  result = result.slice(0, -3);
-  return result;
+  return employees.slice(0, -3);
 }
