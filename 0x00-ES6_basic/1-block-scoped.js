5019b9a48e24c72acd6e92010df65b39b7d7c846
fabrizia2 <melvinrenish56@gmail.com>
2023-04-22 19:16:44 +0300

    block-scoped

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,7 +2,7 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
-  if (trueOrFalse) { /*code holder */}
+  if (trueOrFalse) { /* code holder */ }
 
   return [task, task2];
 }
