131734c3150b2556904a36b4d24e569aa75f05d5
fabrizia2 <melvinrenish56@gmail.com>
2023-04-22 17:53:59 +0300

    Classes

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,12 +1,5 @@
 class Airport {
   constructor(name, code) {
-    if (typeof name !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    if (typeof code !== 'string') {
-      throw new TypeError('Code must be a string');
-    }
-
     this._name = name;
     this._code = code;
   }
@@ -15,27 +8,19 @@ class Airport {
     return this._name;
   }
 
-  set name(newName) {
-    if (typeof newName !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    this._name = newName;
+  set name(name) {
+    this._name = name;
   }
 
   get code() {
     return this._code;
   }
 
-  set code(newCode) {
-    if (typeof newCode !== 'string') {
-      throw new TypeError('Code must be a string');
-    }
-    this._code = newCode;
+  set code(code) {
+    this._code = code;
   }
 
   toString() {
     return this._code;
   }
 }
-
-module.exports = Airport;
