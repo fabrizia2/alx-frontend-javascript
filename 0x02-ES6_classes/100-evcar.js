a4c45fdb4c5fad3219eba8557f8b7005417b45f1
fabrizia2 <melvinrenish56@gmail.com>
2023-04-22 18:16:32 +0300

    Classes

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,43 @@
+// 10-car.js
+
+export class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  cloneCar() {
+    const clone = Object.assign(Object.create(Object.getPrototypeOf(this)), this);
+    return clone;
+  }
+}
+
+export class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  cloneCar() {
+    const clone = super.cloneCar();
+    Object.setPrototypeOf(clone, Car.prototype);
+    return clone;
+  }
+}
