7c0ca79fb574500f9f8aaf8cb071f4b9dfbc747a
fabrizia2 <melvinrenish56@gmail.com>
2023-04-22 21:34:25 +0300

    The wonders

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,6 +1,5 @@
 export default function createEmployeesObject(departmentName, employees) {
-  const obj = {
-    [departmentName]: employees
+  return {
+    [departmentName]: employees,
   };
-  return obj;
 }
