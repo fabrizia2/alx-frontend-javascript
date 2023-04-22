2ed948adb596fcc07400e5cd5faacfe538092385
fabrizia2 <melvinrenish56@gmail.com>
2023-04-22 22:12:25 +0300

    The wonders

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,24 +1,18 @@
 export default function createIteratorObject(report) {
-  const allEmployees = report.allEmployees;
-  const departments = Object.keys(allEmployees);
-  let currentDeptIndex = 0;
-  let currentEmployeeIndex = 0;
-
-  const iterator = {
-    next: function () {
-      if (currentDeptIndex >= departments.length) {
-        return { done: true };
-      }
-      const currentDept = departments[currentDeptIndex];
-      const currentEmployee = allEmployees[currentDept][currentEmployeeIndex];
-      currentEmployeeIndex++;
-      if (currentEmployeeIndex >= allEmployees[currentDept].length) {
-        currentDeptIndex++;
-        currentEmployeeIndex = 0;
-      }
-      return { value: currentEmployee };
+  let allEmployees = report.allEmployees;
+  let flattenedEmployees = [];
+  
+  for (let dept in allEmployees) {
+    flattenedEmployees.push(...allEmployees[dept]);
+  }
+  
+  let currentIndex = 0;
+  
+  return {
+    next: function() {
+      return currentIndex < flattenedEmployees.length ?
+        { value: flattenedEmployees[currentIndex++], done: false } :
+        { done: true };
     }
   };
-
-  return iterator;
 }
