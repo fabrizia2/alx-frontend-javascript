2cc4af0c197914bd73565caf527be5ca830ec385
fabrizia2 <melvinrenish56@gmail.com>
2023-04-22 17:06:15 +0300

    Classes

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,6 +1,13 @@
 import ClassRoom from './0-classroom.js';
 
 function initializeRooms() {
-  const arrays = [19, 20, 34];
-  return arrays.map(arrays => new ClassRoom(arrays));
+  const array = [19, 20, 34];
+  const classrooms = [];
+
+  for (let i = 0; i < array.length; i++) {
+    const classroom = new ClassRoom(array[i]);
+    classrooms.push(classroom);
+  }
+
+  return classrooms;
 }
