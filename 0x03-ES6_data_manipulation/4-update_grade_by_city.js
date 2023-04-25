fe3a0eb8ab7dd5e7441217726a21292509b2edc8
fabrizia2 <melvinrenish56@gmail.com>
2023-04-25 07:21:52 +0300

    Lint

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,5 +1,5 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  const studentsInCity = students.filter(student => student.location === city);
+  const studentsInCity = students.filter((student) => student.location === city);
 
   return studentsInCity.map((student) => {
     const grade = newGrades.find((g) => g.studentId === student.id)?.grade;
