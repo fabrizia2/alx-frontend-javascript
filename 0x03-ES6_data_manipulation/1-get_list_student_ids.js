3cf06b17341cca482e6cac9ac37be2ebf472353c
fabrizia2 <melvinrenish56@gmail.com>
2023-04-25 07:20:22 +0300

    Lint

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -3,6 +3,5 @@ export default function getListStudentIds(students) {
     return [];
   }
 
-
   return students.map((student) => student.id);
 }
--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,8 +1,8 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
   const studentsInCity = students.filter(student => student.location === city);
 
-  return studentsInCity.map(student => {
-    const grade = newGrades.find(g => g.studentId === student.id)?.grade;
+  return studentsInCity.map((student) => {
+    const grade = newGrades.find((g) => g.studentId === student.id)?.grade;
     return { ...student, grade: grade ?? 'N/A' };
   });
 }
